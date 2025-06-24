aab2f0b1c70846fbfe9990068f15264bc81657f4
hithesh0160 <hitheshputhran0160@gmail.com>
2025-06-24 20:08:53 +0530

    fixjfvnfj

--- shop/src/main/java/com/amazon/testscript/TabletsTest.java
+++ shop/src/main/java/com/amazon/testscript/TabletsTest.java
@@ -5,6 +5,7 @@ import com.amazon.constants.AmazonConstants;
 import com.amazon.constants.AmazonLocators;
 import com.amazon.utils.ExtractDataFromResponse;
 import com.amazon.utils.UrlGenerator;
+import org.testng.annotations.Test;
 
 public class TabletsTest {
     // Old main method (commented out)
@@ -24,7 +25,30 @@ public class TabletsTest {
     }
     */
 
+    /*
     public static void main(String[] args) throws Exception {
+        // Use UrlGenerator to parameterize the new Amazon URL structure
+        String url = UrlGenerator.generateUrlWithParams(
+            "tablets", // k
+            "Smartphones", // i
+            AmazonConstants.Discount50PercentOff, // rh
+            "price-asc-rank", // s
+            "dc", // dc
+            "v1%3A1LnjVlgfcmLpzkRTyvVg9drUER1%2BtdJTV1L4PXz3uw4" // ds
+        );
+
+        String htmlBody = HtmlBodyExtractor.getHtmlBodyFromUrl(url);
+        System.out.println("\n--- htmlBody ---");
+        System.out.println(htmlBody);
+
+        // Print the extracted data
+        System.out.println("\n--- Extracted Data ---");
+        ExtractDataFromResponse.extractData(htmlBody, AmazonLocators.productName, AmazonLocators.productPrice);
+    }
+    */
+
+    @Test
+    public void testExtractTabletData() throws Exception {
         // Use UrlGenerator to parameterize the new Amazon URL structure
         String url = UrlGenerator.generateUrlWithParams(
             "tablets", // k
