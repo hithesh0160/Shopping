118fd544fafb3748b1d091e92df08dc0f02d8597
hithesh0160 <hitheshputhran0160@gmail.com>
2025-07-23 20:05:51 +0530

    fix locators 2

--- shop/src/main/java/com/amazon/constants/AmazonLocators.java
+++ shop/src/main/java/com/amazon/constants/AmazonLocators.java
@@ -3,7 +3,7 @@ package com.amazon.constants;
 public class AmazonLocators {
 
     // Product Name Locator
-    public static final String productName = "//a[@class='a-link-normal s-line-clamp-2 s-link-style a-text-normal']";
+    public static final String productName = "//a[@class='a-link-normal s-line-clamp-2 s-line-clamp-3-for-col-12 s-link-style a-text-normal']";
     
     // Product Price Locator
     public static final String productPrice = "//span[@class='a-price-whole']";
