001932f4540ec3cbd398d3b08945f107c350d8c0
hithesh0160 <hitheshputhran0160@gmail.com>
2025-07-24 18:42:12 +0530

    vdbvxngbvzc xvz

--- shop/src/main/java/com/amazon/utils/HtmlBodyExtractor.java
+++ shop/src/main/java/com/amazon/utils/HtmlBodyExtractor.java
@@ -25,7 +25,6 @@ public class HtmlBodyExtractor {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(url))
             .header("Accept", "*/*")
-            .header("Connection", "keep-alive")
             .header("Accept-Encoding", "gzip, deflate")
             .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
             .GET()
