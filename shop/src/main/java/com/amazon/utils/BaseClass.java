2ea3be95a151db0e56dd0675825a8033e98b5304
hithesh0160 <hitheshputhran0160@gmail.com>
2025-07-27 22:29:02 +0530

    fix page load test

--- shop/src/main/java/com/amazon/utils/BaseClass.java
+++ shop/src/main/java/com/amazon/utils/BaseClass.java
@@ -13,6 +13,7 @@ import com.amazon.constants.AmazonConstants;
 import com.amazon.locators.AmazonLocators;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -36,8 +37,11 @@ public class BaseClass {
 		options.addArguments("--no-sandbox");
 		options.addArguments("--headless");
 		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                     "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                     "Chrome/114.0.5735.133 Safari/537.36");
 		options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        options.addArguments("--disable-blink-features=AutomationControlled"); // 👈 helps bypass bot detection
 		
 		Path userDataDir = Files.createTempDirectory(java.util.UUID.randomUUID().toString());
 		options.addArguments("--user-data-dir=" + userDataDir.toString());
@@ -64,12 +68,16 @@ public class BaseClass {
          );
         
 		driver.get(url);
-		System.out.println("Title: "+ driver.getCurrentUrl());
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(300));
+        System.out.println("Title: "+ driver.getCurrentUrl());
+
+        new WebDriverWait(driver, Duration.ofSeconds(60)).until(webDriver -> ((JavascriptExecutor) webDriver)
+        .executeScript("return document.readyState").equals("complete"));
+
+        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(300));
 		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(AmazonLocators.productName)));
         // Thread.sleep(75000); // Adjust sleep time as necessary
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(AmazonLocators.productPrice)));
+		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(AmazonLocators.productPrice)));
     }
 
     @AfterMethod
